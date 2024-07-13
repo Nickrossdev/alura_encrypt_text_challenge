ea9d6d7d3bee06020689ba3f4ae951d58f2b5839
Nickrossdev <roynicolsanchezcerron@gmail.com>
2024-07-12 23:59:07 -0500

    change theme mode app

---
+++ theme.js
@@ -0,0 +1,43 @@
+let systemColorScheme;
+let theme;
+const btn_tema = document.getElementById("btn_tema");
+const root = document.getElementById('root');
+
+function changeAppTheme(){
+    
+    if(theme === 'dark'){
+        root.classList.remove('dark');
+        root.classList.add('light');
+        theme = 'light';
+    } else {
+        root.classList.remove('light');
+        root.classList.add('dark');
+        theme = 'dark';
+    }
+}
+
+function addClassThemeUser(){
+    if (systemColorScheme === 'dark'){
+        root.classList.add('dark');
+    } else {
+        root.classList.add('light');
+    }
+}
+
+// Función para obtener y guardar la preferencia de tema del sistema
+function detectSystemColorSchemeUser() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        systemColorScheme = 'dark';
+    } else {
+        systemColorScheme = 'light';
+    }
+    theme = systemColorScheme;
+}
+
+btn_tema.addEventListener('click', changeAppTheme);
+
+// Llama a la función al cargar la página
+window.addEventListener('load', () => {
+    detectSystemColorSchemeUser();
+    addClassThemeUser();
+});
